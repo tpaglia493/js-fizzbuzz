591516b2ef7c94d3de06d8d8b6b71f44670eea44
tpaglia493 <tommasopaglia93@outlook.it>
2023-03-30 14:48:29 +0200

    better but need debugging

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
-let numberInsideTheBox;
-
-for (i = 0; i <= 100; i++) {
-
-    numberInsideTheBox = i;
-
-    if (numberInsideTheBox % 3 == 0) {
-
-        if (numberInsideTheBox % 5 == 0) {
-            numberInsideTheBox = "FIZZ-BUZZ"
-        }else { numberInsideTheBox = "FIZZ"
-     }
-    
-    }else if (numberInsideTheBox % 5 == 0) {
-         numberInsideTheBox == "BUZZ" }
 
 
+for (let i = 0; i <= 100; i++) {
+
+   
+
+    if (i % 3 == 0) {
+
+        if (i % 5 == 0) {
+            //""
+            //HTML MODIFIER
+            document.getElementById("container-of-boxes").innerHTML += `
+            <div class="col-1 card" style="height:100px; width:100px">
+                <p class="m-auto">FIZZ-BUZZ</p>
+            </div>`
+        } else {
+            //"FIZZ"
+            //HTML MODIFIER
+            document.getElementById("container-of-boxes").innerHTML += `
+            <div class="col-1 card" style="height:100px; width:100px">
+                <p class="m-auto">FIZZ</p>
+            </div>`
+
+        }
+
+    } else if (i % 5 == 0) {
+        //"BUZZ"
+        //HTML MODIFIER
+        document.getElementById("container-of-boxes").innerHTML += `
+    <div class="col-1 card" style="height:100px; width:100px">
+        <p class="m-auto">BUZZ</p>
+    </div>`
+    }
+
+//TO FIX : IT MUST NOT PRINT ì WHEN FIZZ, BUZZ OR FIZZBUZZ
 
     //HTML MODIFIER
     document.getElementById("container-of-boxes").innerHTML += `
     <div class="col-1 card" style="height:100px; width:100px">
-        <p class="m-auto">${numberInsideTheBox}</p>
+        <p class="m-auto">${i}</p>
     </div>`
 }
