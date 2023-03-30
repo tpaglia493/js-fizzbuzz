7c0031da1e1d86ed2ae0c835da0e19b79b86031f
tpaglia493 <tommasopaglia93@outlook.it>
2023-03-30 14:15:43 +0200

    fizzbuzz version - need debugging

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 let numberInsideTheBox;
 
-for(i=1; i<=100; i++){
-    document.getElementById("container-of-boxes").innerHTML+= `
+for (i = 0; i <= 100; i++) {
+
+    numberInsideTheBox = i;
+
+    if (numberInsideTheBox % 3 == 0) {
+
+        if (numberInsideTheBox % 5 == 0) {
+            numberInsideTheBox = "FIZZ-BUZZ"
+        }else { numberInsideTheBox = "FIZZ" }
+    
+    }else if (numberInsideTheBox % 5 == 0) {
+         numberInsideTheBox == "BUZZ" }
+
+
+
+    //HTML MODIFIER
+    document.getElementById("container-of-boxes").innerHTML += `
     <div class="col-1 card" style="height:100px; width:100px">
         <p class="m-auto">${i}</p>
     </div>`
