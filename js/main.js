cd8aa7ab487ce8ea3d3616ba4ca4c00129732fea
tpaglia493 <tommasopaglia93@outlook.it>
2023-03-30 14:56:26 +0200

    fixed - fiz-buzz working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
    
 
     if (i % 3 == 0) {
 
         if (i % 5 == 0) {
-            //""
+            //"FIZZ-BUZZ"
             //HTML MODIFIER
             document.getElementById("container-of-boxes").innerHTML += `
             <div class="col-1 card" style="height:100px; width:100px">
@@ -30,7 +30,7 @@ for (let i = 0; i <= 100; i++) {
     <div class="col-1 card" style="height:100px; width:100px">
         <p class="m-auto">BUZZ</p>
     </div>`
-    }
+    }else{
 
 //TO FIX : IT MUST NOT PRINT ì WHEN FIZZ, BUZZ OR FIZZBUZZ
 
@@ -38,5 +38,5 @@ for (let i = 0; i <= 100; i++) {
     document.getElementById("container-of-boxes").innerHTML += `
     <div class="col-1 card" style="height:100px; width:100px">
         <p class="m-auto">${i}</p>
-    </div>`
+    </div>`}
 }
